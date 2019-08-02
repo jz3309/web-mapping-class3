53bfd27352afd0b915570cf231b8be4289892265
jz3309 <43050480+jz3309@users.noreply.github.com>
2019-08-02 18:48:23 -0400

    make it better

--- .history/js/scripts_20190802172957.js
+++ .history/js/scripts_20190802172957.js
@@ -25,24 +25,24 @@ stationdata.forEach(function(station) {
 });
 
 // get realtime data
-// var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
-// var request = require('request');
+var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+var request = require('request');
 
-// var requestSettings = {
-//     method: 'GET',
-//     url: 'URL OF YOUR GTFS-REALTIME SOURCE GOES HERE',
-//     encoding: null
-// };
-// request(requestSettings, function(error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//         var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
-//         feed.entity.forEach(function(entity) {
-//             if (entity.trip_update) {
-//                 console.log(entity.trip_update);
-//             }
-//         });
-//     }
-// });
+var requestSettings = {
+    method: 'GET',
+    url: 'URL OF YOUR GTFS-REALTIME SOURCE GOES HERE',
+    encoding: null
+};
+request(requestSettings, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+        feed.entity.forEach(function(entity) {
+            if (entity.trip_update) {
+                console.log(entity.trip_update);
+            }
+        });
+    }
+});
 
 
 
